151004c68b376f6a2309026f46ebd50aa23e62f5
azizul96 <alifalamin42@gmail.com>
2023-11-07 03:32:38 +0600

    Create delete method to cancel bookings from my booking page

--- index.js
+++ index.js
@@ -29,6 +29,25 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middleware
+const verifyToken = async(req, res, next)=>{
+  const token = req?.cookies?.token
+  console.log('value of token ', token);
+  if(!token){
+      return res.status(401).send({message: 'Not Authorized'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      console.log(err);
+      return res.status(401).send({message: 'Not Authorized'})
+    }
+    console.log('value in the token', decoded);
+    req.user = decoded
+    next()
+  })
+  
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -58,24 +77,47 @@ async function run() {
       res.clearCookie('token',{maxAge: 0}).send({success: true})
     })
 
-
+// http://localhost:5000/rooms?sortField=price&sortOrder=asc
     // Rooms
     app.get('/rooms', async(req, res)=>{
-      const result = await roomsCollection.find().toArray()
+      let sortObj = {}
+      const sortField = req.query.sortField
+      const sortOrder = req.query.sortOrder
+
+      if(sortField && sortOrder){
+        sortObj[sortField] = sortOrder
+      }
+      const result = await roomsCollection.find().sort(sortObj).toArray()
       res.send(result)
     })
 
     // Bookings
-    app.get('/bookings', async(req, res)=>{
-      const result = await bookingsCollection.find().toArray()
+    app.get('/bookings', verifyToken, async(req, res)=>{
+
+      if(req.query.email !== req.user.email){
+        return res.status(403).send({message: "Forbidden"})
+      }
+      let query = {}
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const result = await bookingsCollection.find(query).toArray()
       res.send(result)
     })
-
+    
+    
     app.post('/bookings', async(req, res)=>{
       const booking = req.body
       const result = await bookingsCollection.insertOne(booking)
       res.send(result)
   
+    })
+
+    app.delete('/bookings/:id', async(req, res)=>{
+      const id = req.params.id
+      const query = { _id: new ObjectId(id)}
+      const result = await bookingsCollection.deleteOne(query)
+      res.send(result)
     })
 
     

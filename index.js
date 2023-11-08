d04b963f90ee5eb69bca2731c07b1727c25f128f
azizul96 <alifalamin42@gmail.com>
2023-11-09 02:30:45 +0600

    deploy the server side on vercel

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173',
+  'https://hotel-booking-3b71d.web.app',
+  'https://hotel-booking-3b71d.firebaseapp.com'
+],
   credentials: true
 }))
 app.use(express.json())
